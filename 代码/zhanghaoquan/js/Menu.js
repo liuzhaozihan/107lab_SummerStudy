e525fbd8d7277bb6e37f8fe2a76f1479e4bdb6a9
张浩权 <469356723@qq.com>
2020-07-12 00:47:54 +0800

    zhanghaoquan

---
+++ 代码/zhanghaoquan/js/Menu.js
@@ -0,0 +1,34 @@
+$(function () {  
+
+    var First = $(".menu_part>li");
+    var Second = $(".menu_part>li>ul>li");
+
+    First.hover(function () {  
+        $(this).children().stop().slideToggle(0);
+    })
+    $(".menu_part>li").mouseover(function () {  
+        this.style.backgroundColor = "#1C97D5";
+    })
+    .mouseout(function () {  
+        this.style.backgroundColor = "#1877B7";
+    })
+
+    Second.css("width","127px").css("height","54px").css("line-height","54px").css("font-size","18px");
+
+    First.mouseover(function () {  
+        this.style.backgroundColor = "#1C97D5";
+        $(this).css("border-left","3px solid #C4302B");
+    })
+    .mouseout(function () {  
+        this.style.backgroundColor = "#1877B7";
+        $(this).css("border-left","3px solid #1877B7");
+    })
+    Second.mouseover(function () {  
+        this.style.backgroundColor = "#1C97D5";
+        $(this).css("border-left","3px solid #C4302B");
+    })
+    .mouseout(function () {  
+        this.style.backgroundColor = "#1877B7";
+        $(this).css("border-left","3px solid #1877B7");
+    })
+})
