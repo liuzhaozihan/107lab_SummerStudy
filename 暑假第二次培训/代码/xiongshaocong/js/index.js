817f39bfcbadd4a37989d5502e4f3298b492cc78
wangyingming <2730105954@qq.com>
2020-07-22 22:44:52 +0800

    xiongshaocong

---
+++ 暑假第二次培训/代码/xiongshaocong/js/index.js
@@ -0,0 +1,10 @@
+$(function () {
+  jQuery('.fullwidthbanner').revolution({
+    delay: 9000,
+    startwidth: 1170,
+    startheight: 450,
+    hideThumbs: 10,
+    fullWidth: "on",
+    forceFullWidth: "on"
+  });
+});
